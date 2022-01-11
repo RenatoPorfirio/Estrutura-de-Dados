9adbc25524808cf54aa173f32dcd00618136de23
RenatoPorfirio <95599633+RenatoPorfirio@users.noreply.github.com>
2022-01-10 23:54:52 -0300

    Update BTree.cpp

--- BTree.cpp
+++ BTree.cpp
@@ -14,10 +14,8 @@ int main(){
 	t.setRoot(new BTNode(1)); //alocando memoria no ponteiro da raiz;
 	
 	BTNode<int>* a = t.getRoot(); //o ponteiro "a" passa a apontar para a raiz da arvore;
-	BTNode<int>* b = 
-	a->set_leftChild(new BTNode(2)); //o ponteiro "b" passa a apontar para o novo espaco de memoria alocado como filho a esquerda do no "a";
-	BTNode<int>* c = 
-	a->set_rightChild(new BTNode(3)); //o ponteiro "c" passa a apontar para o novo espaco de memoria alocado como filho a direita do no "a";
+	BTNode<int>* b = a->set_leftChild(new BTNode(2)); //o ponteiro "b" passa a apontar para o novo espaco de memoria alocado como filho a esquerda do no "a";
+	BTNode<int>* c = a->set_rightChild(new BTNode(3)); //o ponteiro "c" passa a apontar para o novo espaco de memoria alocado como filho a direita do no "a";
 	
 	b->set_leftChild(new BTNode(4)); //adicionando filho a esqueda de b;
 	b->set_rightChild(new BTNode(5)); //adicionando filho a direita de b;
